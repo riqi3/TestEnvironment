35e147691a1f8a2524d9e6cb7120822f4bf6cd00
riqi3 <enrique.pacudan@usjr.edu.ph>
2023-06-14 01:04:52 +0800

    with login and display all messages

--- lib/Home/HomeScreen.dart
+++ lib/Home/HomeScreen.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_sms_inbox/flutter_sms_inbox.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,6 +14,14 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
     final TextEditingController _firstName = TextEditingController();
     final TextEditingController _lastName = TextEditingController();
+
+final SmsQuery _query = SmsQuery();
+  List<SmsMessage> _messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   void clearText(){
     _firstName.clear();
@@ -81,8 +91,71 @@ Future createUser({required String firstName, required String lastName}) async {
               style: TextStyle(),
             ),
           ),
+          Expanded(
+            child: Container(
+            padding: const EdgeInsets.all(10.0),
+            child: _messages.isNotEmpty
+                ? _MessagesListView(
+                    messages: _messages,
+                  )
+                : Center(
+                    child: Text(
+                      'No messages to show.\n Tap refresh button...',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  ),
+          ),
         ],
       ),
+              floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            var permission = await Permission.sms.status;
+            if (permission.isGranted) {
+              final messages = await _query.querySms(
+                kinds: [
+                  SmsQueryKind.inbox,
+                  SmsQueryKind.sent,
+                ],
+                // address: '+254712345789',
+                count: _messages.length,
+              );
+              debugPrint('sms inbox messages: ${messages.length}');
+
+              setState(() => _messages = messages);
+            } else {
+              await Permission.sms.request();
+            }
+          },
+          child: const Icon(Icons.refresh),
+        ),
+    );
+  }
+}
+
+
+class _MessagesListView extends StatelessWidget {
+  const _MessagesListView({
+    Key? key,
+    required this.messages,
+  }) : super(key: key);
+
+  final List<SmsMessage> messages;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: messages.length,
+      itemBuilder: (BuildContext context, int i) {
+        var message = messages[i];
+
+        return ListTile(
+          title: Text('${message.sender} [${message.date}]'),
+          subtitle: Text('${message.body}'),
+        );
+      },
     );
   }
 }
